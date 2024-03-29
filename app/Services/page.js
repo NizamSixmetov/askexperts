cf4ef548a96534675d28f21cdbceb673ea9428d7
Shikhmetov Nizam <134426138+NizamSixmetov@users.noreply.github.com>
2024-03-29 18:48:54 +0400

    Services comp

--- app/Services/page.js
+++ app/Services/page.js
@@ -1,7 +1,23 @@
-import React from "react";
+import HomeServices from "../Components/HomeServices";
+import PageCap from "../Components/PageCap";
+import styles from "./style.module.css";
 
 const services = () => {
-  return <div>services</div>;
+  return (
+    <div>
+      <div className={`${styles.marginDiv}`}>
+        <div className={`${styles.bgDiv}`}>
+          <div className="container">
+            <PageCap
+              pElemnt={"Services"}
+              h2Element={"Creative Services For Boost Your Business Growth"}
+            />
+          </div>
+        </div>
+      </div>
+      <HomeServices />
+    </div>
+  );
 };
 
 export default services;
