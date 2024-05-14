690e2885f0b67afa4535084655187798b4868f4d
Shikhmetov Nizam <134426138+NizamSixmetov@users.noreply.github.com>
2024-05-14 18:39:53 +0400

    fix

--- app/About/page.js
+++ app/About/page.js
@@ -1,7 +1,44 @@
 import React from "react";
+import styles from "./style.module.css";
+import PageCap from "../Components/PageCap";
+import AboutVision from "../Components/AboutVision";
+import ProjectNumber from "../Components/ProjectNumber";
+import AboutClients from "../Components/AboutClients";
+import AboutHistory from "../Components/AboutHistory";
+import AboutTeam from "../Components/AboutTeam";
+import HomeBookNow from "../Components/HomeBookNow";
 
-const page = () => {
-  return <div>page</div>;
+export const metadata = {
+  title: "About",
+  description: "About Page",
+  icons: {
+    icon: "/metadataIcon/about.svg",
+  },
 };
 
-export default page;
+const About = () => {
+  return (
+    <div>
+      <div className={`${styles.marginDiv}`}>
+        <div className={`${styles.bgDiv}`}>
+          <div className="container">
+            <PageCap
+              text="Services"
+              h2Element={"Creative Services For Boost Your Business Growth"}
+            />
+          </div>
+        </div>
+      </div>
+      <AboutVision />
+      <ProjectNumber />
+      <AboutClients />
+      <AboutHistory />
+      {/* <AboutTeam /> */}
+      <div style={{ padding: "4rem 0" }}>
+        <HomeBookNow />
+      </div>
+    </div>
+  );
+};
+
+export default About;
