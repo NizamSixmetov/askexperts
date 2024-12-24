73094fff1f21069703017a8ef6b38f06abf81861
Shikhmetov Nizam <134426138+NizamSixmetov@users.noreply.github.com>
2024-12-24 16:06:16 +0400

    Image Names Fixed

--- app/Components/HomePartner/index.jsx
+++ app/Components/HomePartner/index.jsx
@@ -4,7 +4,7 @@ import styles from "./style.module.css";
 import { Autoplay } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
-import Image from "next/image";
+// import Image from "next/image";
 
 const HomePartner = () => {
   return (
@@ -21,45 +21,43 @@ const HomePartner = () => {
       >
         <SwiperSlide>
           <div className={`${styles.swiper}`}>
-            {/* <img src="/Homepartner/1.svg" alt="TestImage1" /> */}
-            {/* <img src="/public/HomePartner/1.svg" alt="TestImage2" /> */}
-            {/* <img src="../../../public/HomePartner/1.svg" alt="TestImage3" /> */}
-            <Image
+            <img src="/HomePartner/1.svg" alt="TestImage1" />
+            {/* <Image
               src={"/HomePartner/1.svg"}
               alt="TestImage1"
               width={100}
               height={100}
               loading="lazy"
-            />
+            /> */}
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className={`${styles.swiper}`}>
             <img
               className={`${styles.imgDiv}`}
-              src="/Homepartner/2.svg"
+              src="/HomePartner/2.svg"
               alt="Image"
             />
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className={`${styles.swiper}`}>
-            <img src="/Homepartner/3.svg" alt="Image" />
+            <img src="/HomePartner/3.svg" alt="Image" />
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className={`${styles.swiper}`}>
-            <img src="/Homepartner/4.svg" alt="Image" />
+            <img src="/HomePartner/4.svg" alt="Image" />
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className={`${styles.swiper}`}>
-            <img src="/Homepartner/5.svg" alt="Image" />
+            <img src="/HomePartner/5.svg" alt="Image" />
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className={`${styles.swiper}`}>
-            <img src="/Homepartner/6.svg" alt="Image" />
+            <img src="/HomePartner/6.svg" alt="Image" />
           </div>
         </SwiperSlide>
       </Swiper>
