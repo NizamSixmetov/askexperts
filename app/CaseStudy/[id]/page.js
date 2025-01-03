83d8b843e2ee6f2a4bd1b32f6cddf3e40a38c4c5
Shikhmetov Nizam <134426138+NizamSixmetov@users.noreply.github.com>
2025-01-03 19:09:26 +0400

    Add CAseStudy page Loader

--- app/CaseStudy/[id]/page.js
+++ app/CaseStudy/[id]/page.js
@@ -6,6 +6,7 @@ import CaseStudySingleStoryboard from "@/app/Components/CaseStudySingleStoryboar
 import CaseStudyProjectResult from "@/app/Components/CaseStudyProjectResult";
 import TeamSubscribe from "@/app/Components/TeamSubscribe";
 import MoreCase from "@/app/Components/MoreCase";
+import Loader from "@/app/Components/Loader/Loader";
 
 export const metadata = {
   title: "Case",
@@ -35,47 +36,59 @@ const CaseStudySingle = async ({ params: { id } }) => {
       <div className={`${styles.marginDiv}`}>
         <div className={`${styles.bgDiv}`}>
           <div className="container">
-            <div className={`${styles.containerDiv}`}>
-              <p>Case Studies single</p>
-              <h4>{data.name}</h4>
-            </div>
+            {data.id > 0 ? (
+              <div className={`${styles.containerDiv}`}>
+                <p>Case Studies single</p>
+                <h4>{data.name}</h4>
+              </div>
+            ) : (
+              <div className={`${styles.containerDiv}`}>
+                <h4>Not name</h4>
+              </div>
+            )}
           </div>
         </div>
       </div>
       <div className="container">
-        <div style={{ padding: "3rem 0", margin: "0 auto" }}>
-          <div className={`${styles.ImageDiv}`}>
-            <Image src={data.fullImage} fill alt="Image" />
-          </div>
-          <div style={{ position: "relative" }}>
-            <div
-              style={{
-                position: "absolute",
-                top: "-50px",
-                left: "50%",
-                translate: "-50%",
-                width: "100%",
-              }}
-            >
-              <CaseStudyLivePreview data={data} />
+        {data.id > 0 ? (
+          <div style={{ padding: "3rem 0", margin: "0 auto" }}>
+            <div className={`${styles.ImageDiv}`}>
+              <Image src={data.fullImage} fill alt="Image" />
+            </div>
+            <div style={{ position: "relative" }}>
+              <div
+                style={{
+                  position: "absolute",
+                  top: "-50px",
+                  left: "50%",
+                  translate: "-50%",
+                  width: "100%",
+                }}
+              >
+                <CaseStudyLivePreview data={data} />
+              </div>
+            </div>
+            <div style={{ marginTop: "7rem" }}>
+              <CaseStudyProject data={data} />
+            </div>
+            <div>
+              <CaseStudySingleStoryboard data={data} />
+            </div>
+            <div>
+              <CaseStudyProjectResult data={data} />
+            </div>
+            <div>
+              <TeamSubscribe />
+            </div>
+            <div>
+              <MoreCase />
             </div>
           </div>
-          <div style={{ marginTop: "7rem" }}>
-            <CaseStudyProject data={data} />
+        ) : (
+          <div style={{ padding: "1rem" }}>
+            <Loader />
           </div>
-          <div>
-            <CaseStudySingleStoryboard data={data} />
-          </div>
-          <div>
-            <CaseStudyProjectResult data={data} />
-          </div>
-          <div>
-            <TeamSubscribe />
-          </div>
-          <div>
-            <MoreCase />
-          </div>
-        </div>
+        )}
       </div>
     </div>
   );
