1f58d4ac900c3a1a78d140b4e96e34b247fd04bd
Shikhmetov Nizam <134426138+NizamSixmetov@users.noreply.github.com>
2025-01-03 19:32:17 +0400

    Add Loader for Team page

--- app/Components/TeamMeetData/index.jsx
+++ app/Components/TeamMeetData/index.jsx
@@ -4,9 +4,12 @@ import Link from "next/link";
 import { FaInstagram } from "react-icons/fa";
 import { FaFacebook } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa";
+import Loader from "../Loader/Loader";
 
 async function fetchData() {
-  const url = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/teamFullData`);
+  const url = await fetch(
+    `${process.env.NEXT_PUBLIC_BACKEND_URL}/teamFullData`
+  );
   const result = await url.json();
   return result;
 }
@@ -16,48 +19,65 @@ const TeamMeetData = async () => {
   return (
     <div className="container">
       <div className={`${styles.flex}`}>
-        {data.map(
-          ({
-            id,
-            image,
-            name,
-            position,
-            description,
-            instagram,
-            facebook,
-            twitter,
-          }) => {
-            return (
-              <div className={`${styles.workerCard}`}>
-                <section className={`${styles.image}`}>
-                  <Image src={image} fill alt="WorkerImage" />
-                  <p>{position}</p>
-                  <Link
-                    className={`${styles.hrefLink}`}
-                    href={`/Team/${id}`}
-                    prefetch={true}
-                  ></Link>
-                </section>
-                <section className={`${styles.name}`}>
-                  <h4>{name}</h4>
-                </section>
-                <section className={`${styles.description}`}>
-                  <p>{description}</p>
-                </section>
-                <section className={`${styles.social}`}>
-                  <Link target="_blank" href={instagram}>
-                    <FaInstagram />
-                  </Link>
-                  <Link target="_blank" href={facebook}>
-                    <FaFacebook />
-                  </Link>
-                  <Link target="_blank" href={twitter}>
-                    <FaTwitter />
-                  </Link>
-                </section>
-              </div>
-            );
-          }
+        {data.length > 0 ? (
+          data.map(
+            ({
+              id,
+              image,
+              name,
+              position,
+              description,
+              instagram,
+              facebook,
+              twitter,
+            }) => {
+              return (
+                <div className={`${styles.workerCard}`}>
+                  <section className={`${styles.image}`}>
+                    <Image src={image} fill alt="WorkerImage" />
+                    <p>{position}</p>
+                    <Link
+                      className={`${styles.hrefLink}`}
+                      href={`/Team/${id}`}
+                      prefetch={true}
+                    ></Link>
+                  </section>
+                  <section className={`${styles.name}`}>
+                    <h4>{name}</h4>
+                  </section>
+                  <section className={`${styles.description}`}>
+                    <p>{description}</p>
+                  </section>
+                  <section className={`${styles.social}`}>
+                    <Link target="_blank" href={instagram}>
+                      <FaInstagram />
+                    </Link>
+                    <Link target="_blank" href={facebook}>
+                      <FaFacebook />
+                    </Link>
+                    <Link target="_blank" href={twitter}>
+                      <FaTwitter />
+                    </Link>
+                  </section>
+                </div>
+              );
+            }
+          )
+        ) : (
+          <div
+            style={{
+              padding: "1rem",
+              display: "flex",
+              justifyContent: "center",
+              textAlign: "center",
+              flexDirection: "column",
+              gap: "1rem",
+            }}
+          >
+            <Loader />
+            <p style={{ color: "#ffcc4a" }}>Lost connection with server</p>
+            <p style={{ color: "#ffcc4a" }}>Please try again</p>
+          </div>
         )}
       </div>
     </div>
--- app/Team/[id]/page.js
+++ app/Team/[id]/page.js
@@ -6,6 +6,7 @@ import { FaFacebook } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa";
 import { ImLinkedin2 } from "react-icons/im";
 import TeamSubscribe from "@/app/Components/TeamSubscribe";
+import Loader from "@/app/Components/Loader/Loader";
 
 async function fetchData(id) {
   const url = await fetch(
@@ -29,47 +30,64 @@ const TeamSingle = async ({ params: { id } }) => {
     <div>
       <div className={`${styles.marginDiv}`}></div>
       <div className="container">
-        <div className={`${styles.flex}`}>
-          <div className={`${styles.left}`}>
-            <div className={`${styles.imagesDiv}`}>
-              <Image src={data.image} alt="WorkerImage" fill />
-              <img
-                className={`${styles.posImage}`}
-                src="/TeamSingleImage/teamSinglePosition.svg"
-                alt="Image"
-              />
-              <div className={`${styles.frameDiv}`}></div>
-            </div>
-          </div>
-          <div className={`${styles.right}`}>
-            <div className={`${styles.bottomFlex}`}>
-              <div className={`${styles.nameDiv}`}>
-                <h4>{data.name}</h4>
-                <div className={`${styles.topBorder}`}></div>
-                <section className={`${styles.social}`}>
-                  <Link target="_blank" href={data.instagram}>
-                    <FaInstagram />
-                  </Link>
-                  <Link target="_blank" href={data.facebook}>
-                    <FaFacebook />
-                  </Link>
-                  <Link target="_blank" href={data.twitter}>
-                    <FaTwitter />
-                  </Link>
-                  <Link target="_blank" href={data.linkedin}>
-                    <ImLinkedin2 />
-                  </Link>
-                </section>
+        {data.id > 0 ? (
+          <div className={`${styles.flex}`}>
+            <div className={`${styles.left}`}>
+              <div className={`${styles.imagesDiv}`}>
+                <Image src={data.image} alt="WorkerImage" fill />
+                <img
+                  className={`${styles.posImage}`}
+                  src="/TeamSingleImage/teamSinglePosition.svg"
+                  alt="Image"
+                />
+                <div className={`${styles.frameDiv}`}></div>
               </div>
-              <p className={`${styles.firstP}`}>{data.position}</p>
-              <p className={`${styles.twoP}`}>{data.topDescription}</p>
             </div>
-            <div className={`${styles.bottomLine}`}></div>
-            <h4>{data.past}</h4>
-            <p className={`${styles.twoP}`}>{data.bottomDescription1}</p>
-            <p className={`${styles.twoP}`}>{data.bottomDescription2}</p>
+            <div className={`${styles.right}`}>
+              <div className={`${styles.bottomFlex}`}>
+                <div className={`${styles.nameDiv}`}>
+                  <h4>{data.name}</h4>
+                  <div className={`${styles.topBorder}`}></div>
+                  <section className={`${styles.social}`}>
+                    <Link target="_blank" href={data.instagram}>
+                      <FaInstagram />
+                    </Link>
+                    <Link target="_blank" href={data.facebook}>
+                      <FaFacebook />
+                    </Link>
+                    <Link target="_blank" href={data.twitter}>
+                      <FaTwitter />
+                    </Link>
+                    <Link target="_blank" href={data.linkedin}>
+                      <ImLinkedin2 />
+                    </Link>
+                  </section>
+                </div>
+                <p className={`${styles.firstP}`}>{data.position}</p>
+                <p className={`${styles.twoP}`}>{data.topDescription}</p>
+              </div>
+              <div className={`${styles.bottomLine}`}></div>
+              <h4>{data.past}</h4>
+              <p className={`${styles.twoP}`}>{data.bottomDescription1}</p>
+              <p className={`${styles.twoP}`}>{data.bottomDescription2}</p>
+            </div>
           </div>
-        </div>
+        ) : (
+          <div
+            style={{
+              padding: "1rem",
+              display: "flex",
+              justifyContent: "center",
+              flexDirection: "column",
+              textAlign: "center",
+              gap: "1rem",
+            }}
+          >
+            <Loader />
+            <p style={{ color: "#ffcc4a" }}>Lost connection with server</p>
+            <p style={{ color: "#ffcc4a" }}>Please try again later</p>
+          </div>
+        )}
         <TeamSubscribe />
       </div>
     </div>
